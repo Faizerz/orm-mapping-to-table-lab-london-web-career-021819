3757e98c4ee0e1258c2ab06689653fc6b5793c3b
github username <github email address>
2019-02-28 15:53:46 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,36 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.all
+    DB[:conn].execute("SELECT * FROM students")
+  end
+
+  def self.create_table
+    DB[:conn].execute("CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)")
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE students")
+  end
+
+  def save
+    DB[:conn].execute("INSERT INTO students (name, grade) VALUES(?, ?)", self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(name:, grade:)
+    stud = Student.new(name, grade)
+    stud.save
+    stud
+  end
+
 end
